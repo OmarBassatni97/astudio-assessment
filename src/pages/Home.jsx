0c3a1d8891414bc3a0a217275ddaaa1cc33d721e
Omar Bassatni <omarbassatni@gmail.com>
2023-02-19 20:01:08 +0200

    home animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <div className='w-full h-full flex flex-col gap-4 justify-center items-center pt-10'>
-            <h1 className='text-[32px]'>
+            <h1 className='text-[32px] animate-pulse'>
                 Welcome to ASTUDIO Assessment
             </h1>
             <Link to='/products' className='p-2 bg-blue rounded hover:bg-black hover:text-grey duration-500 transition-all'>
